2e1aed7e90d21a2ac359c2e8340601c2b5362504
mkhassan <muhammed.hassan@gmail.com>
2017-05-25 20:16:33 -0400

    escape sequences new line

---
+++ EscapeSequences.java
@@ -0,0 +1,16 @@
+
+public class EscapeSequences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("********\n\n");
+		System.out.print("*      *\n\n");
+		System.out.print("*      *\n\n");
+		System.out.print("*      *\n\n");
+		System.out.print("*      *\n\n");		
+		System.out.print("********");
+		
+	}
+
+}
