cad9f05f49f90710389bf7ef442d653c8ddebd86
mkhassan <muhammed.hassan@gmail.com>
2017-05-25 20:18:02 -0400

    escape sequences new line

---
+++ StringAssignment.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StringAssignment {
+
+	public static void main(String[] args) {
+    
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("enter your name");
+		
+		String name = keyboard.next();
+		
+        System.out.println(name);
+	}
+
+}
